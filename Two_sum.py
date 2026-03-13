7b90d6126c0bd55902c56fd226ab1126138e2738
Chetana Veera Sahithi Mavuri <mavurisahithi@gmail.com>
2026-03-13 15:15:12 +0530

    Implement two_sum function to find indices

---
+++ Two_sum.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target):
+    hashmap = {}
+
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
+
+nums = [2,7,11,15]
+target = 9
+print(two_sum(nums, target))
