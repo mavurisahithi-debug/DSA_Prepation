24fc42a1e2b31eacd1c1b2a6f25319fb5ebd5246
Chetana Veera Sahithi Mavuri <mavurisahithi@gmail.com>
2026-03-14 22:06:14 +0530

    Implement valid_parentheses function to check balance

---
+++ Valid_Parentheses.py
@@ -0,0 +1,15 @@
+def valid_parentheses(s):
+    stack = []
+    mapping = {')':'(', ']':'[', '}':'{'}
+
+    for char in s:
+        if char in mapping:
+            top = stack.pop() if stack else '#'
+            if mapping[char] != top:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
+
+print(valid_parentheses("()[]{}"))
